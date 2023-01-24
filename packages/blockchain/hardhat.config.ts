0bf4f12748b64798f3548e433007f7ac8d49d88b
José Pedro Sousa <github@zepedro.me>
2023-01-24 10:50:24 +0000

    first commit

---
+++ packages/blockchain/hardhat.config.ts
@@ -0,0 +1,11 @@
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-ethers';
+import '@nomicfoundation/hardhat-chai-matchers';
+
+import { HardhatUserConfig } from 'hardhat/config';
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.17',
+};
+
+export default config;
